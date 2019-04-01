0765d6f02791f7175e9db8ba742c9b59f60b7fba
gpmontt <giovanni.perez@velocitymobility.com>
2019-04-01 18:43:19 +0200

    now

--- qt5pibuilder/getgcclinaro.sh
+++ qt5pibuilder/getgcclinaro.sh
@@ -1,7 +1,9 @@
 #!/bin/sh -e
 set -x
-PATH_GCC=/tmp/test
-GCC_VERSION=7.3.1
+# Defaults Values:
+
+PATH_GCC=$1
+GCC_VERSION=$2
 
 USAGE="$(basename "$0") [-h] PATH_GCC GCC_VERSION -- install gcc-linaro-toolchain from https://releases.linaro.org/components/toolchain/binaries/
 
@@ -17,13 +19,9 @@ while [ $# -gt 0 ]; do
 		*)
 			echo "$USAGE"
 			shift
-			exit
 			;;
 	esac	
 done
-
-PATH_GCC=$1
-GCC_VERSION=$2
 
 wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabihf/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz -P $PATH_GCC -O gcc-linaro-$GCC_VERSION.tar.xz
 tar -kx --xz -f gcc-linaro-$GCC_VERSION.tar.xz
