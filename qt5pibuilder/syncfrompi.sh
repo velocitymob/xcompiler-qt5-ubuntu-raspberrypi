deaf751a540f46d8cec1b9324b5f9c560f3800e6
gpmontt <giovanni.perez@velocitymobility.com>
2019-04-06 05:47:51 +0200

    test balenalib/raspberrypi3-debian:stretch to create sysroot

--- qt5pibuilder/syncfrompi.sh
+++ qt5pibuilder/syncfrompi.sh
@@ -1,17 +1,19 @@
-#!/bin/sh
+#!/bin/bash -e
 
-
+set -x
 #SYSROOT_SD= /media/velo/9a7608bd-5bff-4dfc-ac1d-63a956744162mnt/
-SYSROOT_SD= 
-#PATHSYS='pi@192.168.178.53:'
-PATHSYS=$SYSROOT_SD
-
-if [ ! -d sysroot]; then 
+SYSROOT_SD=$1 
+PATHSYS='root@172.17.0.2:'
+#PATHSYS=$SYSROOT_SD
+__KEY=$2
+ls -la
+if [ ! -d $PWD/${SYSROOT_SD} ]; then 
 	mkdir -p sysroot/usr
 	mkdir -p sysroot/opt/vc
 fi
 
-rsync -avz $PATHSYS/lib sysroot
-rsync -avz $PATHSYS/usr/include sysroot/usr
-rsync -avz $PATHSYS/usr/lib sysroot/usr
-rsync -avz $PATHSYS/opt/vc sysroot/opt
+echo ${PATHSYS} ${__KEY}
+rsync -avz -e "ssh -i ${__KEY}" $PATHSYS/lib sysroot
+rsync -avz -e "ssh -i ${__KEY}" $PATHSYS/usr/include sysroot/usr
+rsync -avz -e "ssh -i ${__KEY}" $PATHSYS/usr/lib sysroot/usr
+rsync -avz -e "ssh -i ${__KEY}" $PATHSYS/opt/vc sysroot/opt
