66e5d72d2a36c4f7c521f53bfbd890c4088a58ec
gpmontt <giovanni.perez@velocitymobility.com>
2019-04-07 23:51:44 +0200

    add input variable to select version of gcclinaro

--- qt5pibuilder/getgcclinaro.sh
+++ qt5pibuilder/getgcclinaro.sh
@@ -26,24 +26,24 @@ while getopts 'v:p:?h' option
 do
 	case "${option}" in
 		v)set_variable 	my_gcc_VERSION ${OPTARG}
-			echo $my_gcc_VERSION
+			echo ${my_gcc_VERSION}
 			;;
 		p) set_variable dst_path_GCC ${OPTARG}
-			echo $dst_path_GCC
+			echo ${dst_path_GCC}
 			;;
 		h|?) usage ;;
 	esac	
 done
 
-echo "path: $dst_path_GCC , version: $my_gcc_VERSION"
+echo "path: ${dst_path_GCC} , version: ${my_gcc_VERSION}"
 
-if [[ -z "$dst_path_GCC" || -z "$my_gcc_VERSION" ]]; then
+if [[ -z "${dst_path_GCC}" || -z "${my_gcc_VERSION}" ]]; then
 	dst_path_GCC=/opt/gcc-linaro-7.3.1
 	my_gcc_VERSION=7.3.1
 fi
 
-#wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabihf/gcc-linaro-$my_gcc_VERSION-2018.05-x86_64_arm-linux-gnueabihf.tar.xz  -O gcc-linaro-$my_gcc_VERSION.tar.xz
-#tar -kx --xz -f gcc-linaro-$my_gcc_VERSION.tar.xz
-#mv  gcc-linaro-*  $dst_path_GCC 
-#rm -rf *.tar.*   
+wget -c https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/arm-linux-gnueabihf/gcc-linaro-${my_gcc_VERSION}-2018.05-x86_64_arm-linux-gnueabihf.tar.xz -P ${dst_path_GCC}  -O gcc-linaro-${my_gcc_VERSION}.tar.xz
+tar -kx --xz -f gcc-linaro-${my_gcc_VERSION}.tar.xz
+rm -rf *.tar.*   
+mv  gcc-linaro-*  ${dst_path_GCC} 
 
