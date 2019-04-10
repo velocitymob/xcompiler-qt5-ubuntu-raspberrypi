0c8e4416da5e49ea1613f79ef46b739436e700db
gpmontt <giovanni.perez@velocitymobility.com>
2019-04-10 15:15:24 +0200

    remove autodownload sysroot in build

--- Dockerfile
+++ Dockerfile
@@ -12,11 +12,12 @@ ARG QT_VERSION=5.10
 ARG GCC_VERSION=7.3.1
 ARG PATH_GCC=/opt/gcc-linaro-${GCC_VERSION}
 ARG DEVICE=linux-rasp-pi3-g++
+ARG SYSROOT=/mnt/raspbian/sysroot
 ENV GCC_VERSION ${GCC_VERSION}
 ENV PATH_GCC ${PATH_GCC}
 ENV QT_VERSION ${QT_VERSION}
 ENV ARCHCROSS arm-linux-gnueabihf-
-ENV SYSROOT /mnt/raspbian/sysroot
+ENV SYSROOT ${SYSROOT}
 ENV DEVICE ${DEVICE}
 
 RUN apt-get update && apt-get install -y \
@@ -51,7 +52,7 @@ RUN wget  https://github.com/Kitware/CMake/releases/download/v3.14.1/cmake-3.14.
 	&& cd /opt/cmake* && /bin/bash -c ./bootstrap && make -j $(nproc) &&  make install  \ 
 	&& cmake --version
 	
-# download sysroot from google drive. TODO: find docker 
+# download sysroot from google drive.
 RUN /bin/bash  /opt/qt5pibuilder/getsysroot.sh 
 # download toolchain gcc linaro V7.3.1  TODO: add input to select the version and the compiler .. 
 RUN echo "path: ${PATH_GCC}  Version: ${GCC_VERSION} " \
@@ -63,4 +64,3 @@ RUN printenv && /bin/bash /opt/qt5pibuilder/build.sh -c -d ${DEVICE} -gcc ${GCC_
 
 # show the compiled version  
 RUN /opt/qt5pibuilder/qt5/bin/qmake -query > /opt/reportfile.txt
-#EXPOSE 9876
--- qt5pibuilder/build.sh
+++ qt5pibuilder/build.sh
@@ -9,6 +9,7 @@ unset QT_VERSION GCC_VERSION PATH_GCC
 #QT_VERSION=5.13
 # here come wich gcc tool do you want to try
 #GCC_VERSION=7.3.1
+# SYSROOT=/mnt/raspbian/sysroot
 
 # module used to compile Stationpedelec, in the future add qtweb
 ARCHCROSS=arm-linux-gnueabihf-
@@ -18,7 +19,6 @@ CLEAN=true
 # here you can use a frisch raspbian image or your personal sysroot
 DEVICE="linux-rasp-pi3-g++"
 # DEVICE='linux-rasp-pi3-vc4-g++'
-SYSROOT=/mnt/raspbian/sysroot
 MAKE_OPTS=-j$( nproc)
 USAGE="$(basename "$0") [-c] [-d device] [-gcc GCC_VERSION] [-sys SYSROOT_PATH ] [-qt QT_VERSION] [-h]-- install toolchain and build qt5 for raspberry pi, automated version of https://wiki.qt.io/RaspberryPi2EGLFS
 
@@ -73,6 +73,7 @@ echo "-c:${CLEAN} -d:${DEVICE} -gcc:${GCC_VERSION} -sys:${SYSROOT} -qt:${QT_VERS
 # get compiler
 PATH_GCC=/opt/gcc-linaro-${GCC_VERSION}
 COMPILER=${PATH_GCC}/gcc-linaro-${GCC_VERSION}-2018.05-x86_64_arm-linux-gnueabihf/bin/${ARCHCROSS}
+
 echo "GET COMPILER"
 if [ ! -d ${PATH_GCC}/gcc-linaro-${GCC_VERSION}-2018.05-x86_64_arm-linux-gnueabihf]; then
 	cd /tmp
@@ -85,18 +86,14 @@ fi
 
 
 echo "GET SYSROOT"
-if [ ! -d "$SYSROOT" ]; then
+if [ -d "$SYSROOT" ]; then
 	#TODO: mount  new image and install dependency to create a new sysroot_2019
-	cd /tmp
-	wget -c https://downloads.raspberrypi.org/raspbian_latest -o raspbian.zip
-	unzip raspbian.zip -d /tmp/mysysroot_$(date -u "+%Y.%m.%d-%T")
-  set SYSROOT=$PWD	
-	cd $BASEDIR
+	echo "Getting Sysroot from scratch"
 else
 	echo "Getting Sysroot from googledrive"
-	cd $BASEDIR
 fi
 
+cd $BASEDIR
 # clean old build
 if [ "$CLEAN" = true ]; then
 	rm -rf $BASEDIR/qt5 $BASEDIR/qt5pi
